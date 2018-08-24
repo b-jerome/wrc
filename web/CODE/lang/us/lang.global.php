efef77ff04c03eeb5971dabe46405949708f1937
Jerome <contact@correlatif.com>
2018-08-24 02:55:11 +0200

    Add files via upload

---
+++ web/CODE/lang/us/lang.global.php
@@ -0,0 +1,69 @@
+<?php
+
+//functions.php
+define('LANG_dataok', "Datas loaded!");
+define('LANG_datanotok',"Bad datas in");
+define('LANG_impossible', "Can't open"); 
+define('LANG_impossiblee', "can't write in");
+define('LANG_impossibler', "Can't read");
+define('LANG_connect', "Failed to connect to the controller");
+define('LANG_nodata', "No datas in");
+define('LANG_writeend', "finished writing");
+define('LANG_perms', "is not writable ... Check write permissions to the file");
+
+//controller.php
+define('LANG_max' , "MAX");
+define('LANG_min' , "MIN");
+define('LANG_al' , "Alarm");
+define('LANG_al2_on' , "Alarm sensor 2 ON");
+define('LANG_al2_off' , "Alarm sensor 2 OFF");
+define('LANG_al_on' , "Alarm sensor 1 ON");
+define('LANG_al_off' , "Alarm sensor 1 OFF");
+define('LANG_ac_off' , "Cloud auto OFF");
+define('LANG_as_off' , "Storm auto OFF");
+define('LANG_ac_on' , "Cloud auto ON");
+define('LANG_as_on' , "Storm auto ON");
+define('LANG_restart' , "Restart");
+define('LANG_asc_title' , "Lighting");
+define('LANG_state' , "STATE");
+define('LANG_vitesse' , "SPEED");
+define('LANG_angle' , "ANGLE");
+define('LANG_oscillo_title' , "SERVOS");
+define('LANG_other_title' , "Other");
+define('LANG_svo_on' , "Servo 1 ON");
+define('LANG_svo_off' , "Servo 1 OFF");
+define('LANG_svo2_on' , "Servo 2 ON");
+define('LANG_svo2_off' , "Servo 2 OFF");
+define('LANG_dashboard', "Dashboard");
+define('LANG_light', "Lighting");
+define('LANG_opts', "Options");
+define('LANG_datec', "CONTROLLER DATE");
+define('LANG_tempb', "Tank temperature");
+define('LANG_tempp', "Room temperature");
+define('LANG_color', "Color");
+define('LANG_dur', "length");
+define('LANG_pui', "strength");
+define('LANG_start', "Startup");
+define('LANG_blue', "Royal Blue");
+define('LANG_white', "White");
+define('LANG_hyb', "Hybrid");
+define('LANG_red', "Red");
+define('LANG_green', "Green");
+define('LANG_uv', "UV");
+define('LANG_sim_c_on' , "Simulate cloud ON");
+define('LANG_sim_c_off', "Simulate cloud OFF");
+define('LANG_sim_or_on', "Simulate storm ON");
+define('LANG_sim_or_off', "Simulate storm OFF");
+define('LANG_man_on', "Mode manual ON");
+define('LANG_man_off', "Mode manual OFF");
+define('LANG_tcloud', "Cloud");
+define('LANG_tstorm', "Storm");
+define('LANG_min', "minutes");
+define('LANG_dmin', "Time in minutes");
+define('LANG_saison', "Season");
+define('LANG_temp', "Temperatures");
+define('LANG_aqua', "Tank");
+define('LANG_piece', "Room");
+define('LANG_to', "to");
+define('LANG_de', "from");
+?>
